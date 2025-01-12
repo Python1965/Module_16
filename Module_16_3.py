d97b5dd9667c7bfa509020761c66f5a534efabf4
Python1965 <python65@rambler.ru>
2025-01-12 11:26:27 +0300

    Add files via upload

--- Module_16_3.py
+++ Module_16_3.py
@@ -49,7 +49,7 @@ from fastapi import FastAPI, Path
 from typing import Annotated
 
 
-users_db = {1: {"Имя": "Example", "возраст": 18}}
+users_db = {'1': {"Имя": "Example", "возраст": 18}}
 
 # Создаем экземпляр приложения FastAPI
 app = FastAPI()
@@ -72,7 +72,7 @@ async def get_user_data(username: Annotated[str, Path(..., min_length=5, max_len
 
 
 @app.get("/user/{user_id}")
-async def get_user(user_id: Annotated[int, Path(..., ge=1, le=100, title="User ID", description="Enter User ID", examples=1)]) -> dict:
+async def get_user(user_id: Annotated[str, Path(..., title="User ID", description="Enter User ID", examples=1)]) -> dict:
     return {"user_id": f"Вы вошли как пользователь № {user_id}"}
 
 
@@ -85,13 +85,14 @@ async def get_users() -> dict:
 async def create_user(username: Annotated[str, Path(..., min_length=5, max_length=20, description="Enter username", examples="UrbanUser")],
                       age: Annotated[int, Path(..., ge=18, le=120, description="Enter age", examples=24)]) -> str:
 
-    curr_ind = max(users_db, key=int) + 1
+    # текущий индекс = (находим номер последней строки БД и прибавляем один
+    curr_ind = str(int(max(users_db, key=int)) + 1)
     users_db[curr_ind] = {"Имя": username, "возраст": age}
     return f"User {curr_ind} is registered"
 
 
 @app.put('/user/{user_id}/{username}/{age}')
-async def update_user(user_id: Annotated[int, Path(..., ge=1, le=100, title="User ID", description="Enter User ID", examples=1)],
+async def update_user(user_id: Annotated[str, Path(..., title="User ID", description="Enter User ID", examples='1')],
                       username: Annotated[str, Path(..., min_length=5, max_length=20, description="Enter username", examples="UrbanUser")],
                       age: Annotated[int, Path(..., ge=18, le=120, description="Enter age", examples=24)]) -> str:
 
@@ -100,7 +101,7 @@ async def update_user(user_id: Annotated[int, Path(..., ge=1, le=100, title="Use
 
 
 @app.delete('/user/{user_id}')
-async def update_user(user_id: Annotated[int, Path(..., ge=1, le=100, title="User ID", description="Enter User ID", examples=1)]) -> str:
+async def delete_user(user_id: Annotated[str, Path(..., title="User ID", description="Enter User ID", examples=1)]) -> str:
 
     users_db.pop(user_id)
     return  f"User {user_id} has been deleted"
